d351929b2eea7a6713e1743d1f02deeb5e7f6a05
srujan surapaneni <srujansurapaneni@hotmail.com>
2020-08-13 00:45:24 -0400

    Update feed.py

--- feed.py
+++ feed.py
@@ -2,6 +2,7 @@ import serial
 import json
 
 #replace the string inside brackets with actual COM serial port on windows (use MODE command)
+# on mac/linux, to get the serial ports use this command ls /dev/tty.usb*
 s = serial.Serial('/dev/tty.usbmodem14101')
 
 while True:
